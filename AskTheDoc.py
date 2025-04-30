9179294775e4d49b12b5a2f9c0351a40d8c04a9b
Will Ternes <112189680+Bookswill@users.noreply.github.com>
2025-04-30 16:28:20 -0500

    Update AskTheDoc.py
    UNdoing, no change

--- AskTheDoc.py
+++ AskTheDoc.py
@@ -4,7 +4,6 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-import openai
 
 
 def generate_response(uploaded_file, openai_api_key, query_text):
@@ -33,9 +32,6 @@ def generate_response(uploaded_file, openai_api_key, query_text):
 # Page title
 st.set_page_config(page_title='Ask the Doc App')
 st.title('Ask the Doc App')
-
-# Ensure proxies aren't passed
-openai.proxy = None
 
 # File upload
 uploaded_file = st.file_uploader('Upload an article', type='txt')
