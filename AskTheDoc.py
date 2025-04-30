bcf29a6222c34bf5602794622f0644b655954e65
Will Ternes <112189680+Bookswill@users.noreply.github.com>
2025-04-30 14:09:20 -0500

    Update AskTheDoc.py

--- AskTheDoc.py
+++ AskTheDoc.py
@@ -30,15 +30,14 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     return qa.run(query_text)
 
 # Page title
-st.set_page_config(page_title='🦜🦜 Ask the Doc App')
-st.title('🦜🦜 Ask the Doc App')
+st.set_page_config(page_title='Ask the Doc App')
+st.title('Ask the Doc App')
 
 # File upload
 uploaded_file = st.file_uploader('Upload an article', type='txt')
 
 # Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short
-summary.', disabled=not uploaded_file)
+query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
 # Form input and query
 result = []
