bad2959463673cd1ac0e8a7b60aa98e4cb8c5d03
Will Ternes <112189680+Bookswill@users.noreply.github.com>
2025-04-30 16:06:34 -0500

    Update AskTheDoc.py
    using the from langchain_openai to match the versions being used

--- AskTheDoc.py
+++ AskTheDoc.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
