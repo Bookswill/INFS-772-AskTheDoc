e392e844d77d1a624bd9ab2ba273ec7464046528
Will Ternes <112189680+Bookswill@users.noreply.github.com>
2025-05-01 09:36:39 -0500

    Update AskTheDoc.py
    adding as sys and pysqlite3 import and code line

--- AskTheDoc.py
+++ AskTheDoc.py
@@ -4,7 +4,10 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+import sys
+import pysqlite3
 
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
